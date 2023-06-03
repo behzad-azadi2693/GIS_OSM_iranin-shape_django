c0293fc5042472a3d8c2dd46424aada3f0dfae57
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2023-06-03 16:13:35 +0330

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -2,13 +2,53 @@ from django.contrib import admin
 from leaflet.admin import LeafletGeoAdminMixin
 
 # Register your models here.
-from .models import Roads, Building
+from .models import ( 
+    Buildings, Landuse, Natrual, Places, Pofw, Railway, 
+    Roads, Traffic, Transport, Water, Waterways
+    )
 
-class RoadsAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
-    ordering = ('-geom', )
 
 class BuildingAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
     ordering = ('-geom', )
 
+class LanduseAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class NatrualAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class PlacesAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class PofwAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class RailwayAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class TrafficAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class TransportAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class WaterAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class WaterwaysAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
+class RoadsAdmin(LeafletGeoAdminMixin, admin.ModelAdmin):
+    ordering = ('-geom', )
+
 admin.site.register(Roads, RoadsAdmin)
-admin.site.register(Building, BuildingAdmin)
+admin.site.register(Buildings, BuildingAdmin)
+admin.site.register(Landuse, LanduseAdmin)
+admin.site.register(Natrual, NatrualAdmin)
+admin.site.register(Places, PlacesAdmin)
+admin.site.register(Pofw, PofwAdmin)
+admin.site.register(Railway, RailwayAdmin)
+admin.site.register(Traffic, TrafficAdmin)
+admin.site.register(Transport, TransportAdmin)
+admin.site.register(Water, WaterAdmin)
+admin.site.register(Waterways, WaterwaysAdmin)
